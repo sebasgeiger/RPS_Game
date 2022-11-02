89ccb0d4458cf7b0446d74d6246a30d7e5e57cd6
Sebastian Geiger <geigersr@clarkson.edu>
2022-11-02 14:37:52 -0400

    added js for rock paper scissors game

--- js/page.js
+++ js/page.js
@@ -1,1 +1,69 @@
+function initPick() {
+    var init = "svg/mystery.svg";
+    document.getElementById('oppResult').src = init;   
+}
 
+function rockPick() {
+    function genPick() {
+        var pick = "svg/mystery.svg";
+        if (Math.floor(Math.random() * 3) == 1) {
+            var pick = "svg/rock.svg";
+        } else if (Math.floor(Math.random() * 3) == 2) {
+            var pick = "svg/paper.svg";
+        } else {
+            var pick = "svg/scissors.svg";
+        }
+        document.getElementById('oppResult').src = pick;
+        if (pick == "svg/rock.svg") {
+            document.getElementById('result').innerHTML = "It's a Tie";
+        } else if (pick == "svg/paper.svg") {
+            document.getElementById('result').innerHTML = "You Lose";
+        } else {
+            document.getElementById('result').innerHTML = "You Win";
+        }
+    }
+    genPick();
+}
+
+function paperPick() {
+    function genPick() {
+        var pick = "svg/mystery.svg";
+        if (Math.floor(Math.random() * 3) == 1) {
+            var pick = "svg/rock.svg";
+        } else if (Math.floor(Math.random() * 3) == 2) {
+            var pick = "svg/paper.svg";
+        } else {
+            var pick = "svg/scissors.svg";
+        }
+        document.getElementById('oppResult').src = pick;
+        if (pick == "svg/rock.svg") {
+            document.getElementById('result').innerHTML = "It's a Tie";
+        } else if (pick == "svg/scissors.svg") {
+            document.getElementById('result').innerHTML = "You Lose";
+        } else {
+            document.getElementById('result').innerHTML = "You Win";
+        }
+    }
+    genPick();
+}
+function scissorsPick() {
+    function genPick() {
+        var pick = "svg/mystery.svg";
+        if (Math.floor(Math.random() * 3) == 1) {
+            var pick = "svg/rock.svg";
+        } else if (Math.floor(Math.random() * 3) == 2) {
+            var pick = "svg/paper.svg";
+        } else {
+            var pick = "svg/scissors.svg";
+        }
+        document.getElementById('oppResult').src = pick;
+        if (pick == "svg/scissors.svg") {
+            document.getElementById('result').innerHTML = "It's a Tie";
+        } else if (pick == "svg/scissors.svg") {
+            document.getElementById('result').innerHTML = "You Lose";
+        } else {
+            document.getElementById('result').innerHTML = "You Win";
+        }
+    }
+    genPick();
+}
