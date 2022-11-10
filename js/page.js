8e852d5f50b824872ede538bf9bda0d29c3b14b8
Sebastian Geiger <geigersr@clarkson.edu>
2022-11-10 16:58:14 -0500

    scoreboard working

--- js/page.js
+++ js/page.js
@@ -33,13 +33,13 @@ function rockPick() {
         document.getElementById('oppResult').src = pick;
         if (pick == "svg/rock.svg") {
             document.getElementById('result').innerHTML = "It's a Tie";
-            ++tscore
+            ++tscore;
         } else if (pick == "svg/paper.svg") {
             document.getElementById('result').innerHTML = "You Lose";
-            ++oscore
+            ++oscore;
         } else {
             document.getElementById('result').innerHTML = "You Win";
-            ++pscore
+            ++pscore;
         }
         sessionStorage.setItem("winCount", pscore);
         sessionStorage.setItem("tieCount", tscore);
@@ -52,6 +52,24 @@ function rockPick() {
 }
 function paperPick() {
     function genPick() {
+        let pscore = sessionStorage.getItem("winCount");
+        let tscore = sessionStorage.getItem("tieCount");
+        let oscore = sessionStorage.getItem("lossCount");
+        if (pscore == null) {
+            pscore = 0;
+        } else {
+            pscore = pscore;
+        };
+        if (tscore == null) {
+            tscore = 0;
+        } else {
+            tscore = tscore;
+        };
+        if (oscore == null) {
+            oscore = 0;
+        } else {
+            oscore = oscore;
+        };
         var pick = "svg/mystery.svg";
         if (Math.floor(Math.random() * 3) == 1) {
             var pick = "svg/rock.svg";
@@ -63,16 +81,43 @@ function paperPick() {
         document.getElementById('oppResult').src = pick;
         if (pick == "svg/paper.svg") {
             document.getElementById('result').innerHTML = "It's a Tie";
+            ++tscore;
         } else if (pick == "svg/scissors.svg") {
             document.getElementById('result').innerHTML = "You Lose";
+            ++oscore;
         } else {
             document.getElementById('result').innerHTML = "You Win";
+            ++pscore;
         }
+        sessionStorage.setItem("winCount", pscore);
+        sessionStorage.setItem("tieCount", tscore);
+        sessionStorage.setItem("lossCount", oscore);
+        document.getElementById('pscore').innerHTML = sessionStorage.getItem("winCount");
+        document.getElementById('tscore').innerHTML = sessionStorage.getItem("tieCount");
+        document.getElementById('oscore').innerHTML = sessionStorage.getItem("lossCount");
     }
     genPick();
 }
 function scissorsPick() {
     function genPick() {
+        let pscore = sessionStorage.getItem("winCount");
+        let tscore = sessionStorage.getItem("tieCount");
+        let oscore = sessionStorage.getItem("lossCount");
+        if (pscore == null) {
+            pscore = 0;
+        } else {
+            pscore = pscore;
+        };
+        if (tscore == null) {
+            tscore = 0;
+        } else {
+            tscore = tscore;
+        };
+        if (oscore == null) {
+            oscore = 0;
+        } else {
+            oscore = oscore;
+        };
         var pick = "svg/mystery.svg";
         if (Math.floor(Math.random() * 3) == 1) {
             var pick = "svg/rock.svg";
@@ -84,11 +129,30 @@ function scissorsPick() {
         document.getElementById('oppResult').src = pick;
         if (pick == "svg/scissors.svg") {
             document.getElementById('result').innerHTML = "It's a Tie";
+            ++tscore;
         } else if (pick == "svg/rock.svg") {
             document.getElementById('result').innerHTML = "You Lose";
+            ++oscore;
         } else {
             document.getElementById('result').innerHTML = "You Win";
+            ++pscore;
         }
+        sessionStorage.setItem("winCount", pscore);
+        sessionStorage.setItem("tieCount", tscore);
+        sessionStorage.setItem("lossCount", oscore);
+        document.getElementById('pscore').innerHTML = sessionStorage.getItem("winCount");
+        document.getElementById('tscore').innerHTML = sessionStorage.getItem("tieCount");
+        document.getElementById('oscore').innerHTML = sessionStorage.getItem("lossCount");
     }
     genPick();
 }
+function reset() {
+    sessionStorage.setItem("winCount", 0);
+    sessionStorage.setItem("tieCount", 0);
+    sessionStorage.setItem("lossCount", 0);
+    document.getElementById('pscore').innerHTML = sessionStorage.getItem("winCount");
+    document.getElementById('tscore').innerHTML = sessionStorage.getItem("tieCount");
+    document.getElementById('oscore').innerHTML = sessionStorage.getItem("lossCount");
+    document.getElementById('o-img').src = "svg/mystery.svg";
+    document.getElementById('result').innerHTML = "Result";
+  }
