d4da2edf1d59719c57c7178c3068263be3b221ee
Sebastian Geiger <geigersr@clarkson.edu>
2022-11-10 12:13:14 -0500

    attempt score in rock section

--- js/page.js
+++ js/page.js
@@ -4,6 +4,24 @@ function initPick() {
 }
 function rockPick() {
     function genPick() {
+        let pscore = sessionStorage.getItem("winCount");
+        let tscore = sessionStorage.getItem("tieCount");
+        let oscore = sessionStorage.getItem("lossCount");
+        if (pscore == null) {
+            pscore = 0;
+        } else {
+            pscore = pscore;
+        };
+        if (tscore == null) {
+            tscore = 0;
+        } else {
+            tscore = tscore;
+        };
+        if (oscore == null) {
+            oscore = 0;
+        } else {
+            oscore = oscore;
+        };
         var pick = "svg/mystery.svg";
         if (Math.floor(Math.random() * 3) == 1) {
             var pick = "svg/rock.svg";
@@ -15,11 +33,20 @@ function rockPick() {
         document.getElementById('oppResult').src = pick;
         if (pick == "svg/rock.svg") {
             document.getElementById('result').innerHTML = "It's a Tie";
+            ++tscore
         } else if (pick == "svg/paper.svg") {
             document.getElementById('result').innerHTML = "You Lose";
+            ++oscore
         } else {
             document.getElementById('result').innerHTML = "You Win";
+            ++pscore
         }
+        sessionStorage.setItem("winCount", pscore);
+        sessionStorage.setItem("tieCount", tscore);
+        sessionStorage.setItem("lossCount", oscore);
+        document.getElementById('pscore').innerHTML = sessionStorage.getItem("winCount");
+        document.getElementById('tscore').innerHTML = sessionStorage.getItem("tieCount");
+        document.getElementById('oscore').innerHTML = sessionStorage.getItem("lossCount");
     }
     genPick();
 }
